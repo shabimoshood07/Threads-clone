00081e277818b00541860ed7c47239db6ddb82e6
Moshood Olawale <shabimoshood07@gmail.com>
2023-10-02 23:32:37 +0100

    progress of work

--- components/CreatePost.vue
+++ components/CreatePost.vue
@@ -7,6 +7,46 @@
                 </button>
                 <div class="text-[16px] font-semibold">New Thread</div>
             </div>
+            <div id="Post" class="z-40 bottom-0 max-h-[calc(100vh-200px)] w-full px-3 max-w-[500px] mx-auto">
+                <div class="py-2 w-full">
+                    <div class="flex items-center">
+                        <div class="flex items-center text-white">
+                            <img src="https://picsum.photos/id/223/50" class="rounded-full h-[35px] " alt="">
+                            <h1 class="ml-2 font-semibold text-[18px]"> Mosh Code</h1>
+                        </div>
+                    </div>
+                    <div class="relative flex items-center w-full">
+                        <div class="w-[24px] mx-auto">
+                            <div class="absolute ml-4 mt-1 top-0 w-[1px] bg-gray-700 h-full" />
+                        </div>
+                        <div class="bg-black rounded-lg w-[calc(100%-50px)] text w-full font-light">
+                            <div class="pl-2 text-gray-300 bg-black w-full">
+                                <textarea v-model="text" style="resize: none;" placeholder="start a thread" id="textarea"
+                                    @input="adjustTextareaHeight()" class="w-full bg-black outline-none"></textarea>
+                            </div>
+                            <div class="w-full">
+                                <div class="flex flex-col gap-2 py-1">
+                                    <div v-if="fileDisplay">
+                                        <img :src="fileDisplay" class="mx-auto w-full mt-2 mr-2 rounded-lg" alt="">
+                                    </div>
+                                    <label for="fileInput">
+                                        <Icon name="clarity:paperclip-line" color="#ffffff" size="25" />
+                                        <input type="file" ref="file" id="fileInput" @input="onChange" hidden
+                                            accept=".jpg,.jpeg,.png" />
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="fixed bottom-0 font-bold text-lg w-full p-2 bg-black inline-block border-t border-t-gray-700"
+                v-if="text" :disabled="isLoading" :class="isLoading ? 'text-gray-600' : 'text-blue-600'">
+                <p v-if="!isLoading">Post</p>
+                <p v-else class="flex items-center gap-2 justify-center">
+                    <Icon name="eos-icons:bubble-loading" size="25" />Please wait...
+                </p>
+            </button>
         </div>
     </div>
 </template>
---
+++ pages/auth.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-full h-screen h-[100vh] pt-32">
+        <div class="w-full">
+            <div class="w-full flex items-center justify-center gap-2.5 p-2">
+                <img src="/threads-logo.png" class="w-[35px]" alt="">
+                <span class="font-bold text-2xl text-white">Threads</span>
+            </div>
+        </div>
+
+        <div class="max-w-[350px] mx-auto px-2 text-white">
+            <h1 class="text-center mb-6 mt-4 ">Login / Register</h1>
+            <button class="flex items-center justify-center gap-3 p-1.5 w-full border rounded-full text-lg font-semibold">
+                <div class="flex items-center gap-2 justify-center">
+                    <img src="/github-logo.png" class="w-full max-w-[30px] rounded-full" alt=""> Github
+                </div>
+            </button>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
